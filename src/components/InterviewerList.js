cf2363c1ae38926403d7a62cf71ff9f2916523d2
Robert Beckett <robert.beckett@onion.red>
2019-11-25 14:25:39 -0800

    Removed unused classNames import.

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import classNames from 'classnames';
 
 import "components/InterviewerListItem.scss";
 
 import InterviewerListItem from 'components/InterviewerListItem';
 
 export default function InterviewerList({
-  interviewers, interviewer, setInterviewer
+  interviewers, value, onChange
 }) {
   return (
     <section className="interviewers">
@@ -16,10 +15,11 @@ export default function InterviewerList({
       <ul className="interviewers__list">
         {interviewers.map((current) => {
           return <InterviewerListItem
+            key={current.id}
             name={current.name}
             avatar={current.avatar}
-            selected={current.id === interviewer}
-            setInterviewer={(event) => setInterviewer(current.id)}
+            selected={current.id === value}
+            setInterviewer={(event) => onChange(current.id)}
           />
         })}
       </ul>
