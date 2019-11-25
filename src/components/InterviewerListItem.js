a76b41f600d370a5ee578cff6d2e0e53de6d1502
Robert Beckett <robert.beckett@onion.red>
2019-11-25 13:34:32 -0800

    Implemented InterviewerListItem component.

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,30 @@
+import React from "react";
+import classNames from "classnames";
+
+import "components/InterviewerListItem.scss";
+
+export default function InterviewerListItem({
+  id, name, avatar, selected, setInterviewer
+}) {
+
+  const interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected": selected,
+  });
+
+  const intImageClass = classNames("interviewers__item-image", {
+    "interviewers__item--selected-image": selected,
+  });
+
+  return (
+    <li className={interviewerClass} onClick={
+      () => setInterviewer
+    }>
+      <img
+        className={intImageClass}
+        src={avatar}
+        alt={name}
+      />
+      {selected ? name : ''}
+    </li>
+  );
+}
