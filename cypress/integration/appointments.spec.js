1cfc393b6e7b9183cc936dad587d1579d5acddc9
Robert Beckett <robert.beckett@onion.red>
2019-12-03 13:44:22 -0800

    Implemented should edit an interview E2E test.

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -22,6 +22,19 @@ describe('Appointments', () => {
   });
 
   it('should edit an interview', () => {
+    cy.get('[alt="Edit"]')
+      .click({force: true});
 
+    cy.get('[data-testid="student-name-input"]')
+      .clear().type("Frank Ferrell");
+
+    cy.get('[alt="Tori Malcolm"')
+      .click();
+
+    cy.contains('Save')
+      .click();
+
+    cy.contains('.appointment__card--show', 'Frank Ferrell')
+    .contains('.appointment__card--show', 'Tori Malcolm');
   });
 });
